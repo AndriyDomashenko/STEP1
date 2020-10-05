157b7f7fe532544eb17e57ea28633118cb405233
Andriy <andrikdomik2002@gmail.com>
2020-10-06 00:44:18 +0300

    project1_2

--- script.js
+++ script.js
@@ -11,11 +11,34 @@ const personalmovieDB ={
 
 };
 
-const a = prompt('Один из последних просмотреных фильмов?',''),
-      b = prompt('На сколько  оцените его?',''),
-      c = prompt('Один из последних просмотреных фильмов?',''),
-      d = prompt('На сколько  оцените его?','');           
+          
      
-      personalmovieDB.movies[a] = b;
-      personalmovieDB.movies[c] = d;
-      console.log(personalmovieDB);
+    
+     
+for(let i = 0; i < 2; i++){
+    const a = prompt('Один из последних просмотреных фильмов?',''),
+      b = prompt('На сколько  оцените его?','');
+
+      if(a != null && b != null && a != '' && b != '' && a.length <50 ){
+        personalmovieDB.movies[a] = b;
+        console.log('done');
+      }else{
+    console.log('eror');
+        i--;
+
+}
+}
+if(personalmovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+}else if(personalmovieDB.count >= 10 && personalmovieDB.count < 30){
+    console.log('Вы класичесский зритель');
+}else if(personalmovieDB.count >= 30 ){
+    console.log('Вы киноман');
+}else{
+    console.log('ERROR');
+}
+
+console.log( personalmovieDB);
+
+
+      
